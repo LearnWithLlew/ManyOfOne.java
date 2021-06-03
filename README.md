bbd570271ca57793960e3466c0206142d0792200
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-03 11:03:36 -0600

    init with exercise1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Many Of One - Exercises in java
+
+[![Build Status](../../workflows/build/badge.svg?branch=master)](../../actions?query=build%3Amaster)
+
+
+## Starting Point
+
+The tests in [SampleTests](https://github.com/approvals/ApprovalTests.java.StarterProject/blob/master/src/test/java/org/samples/SampleTests.java) should pass, and then you should delete/modify as needed to get started.
+
+
+Notes:
+
+See the [talk resources](https://github.com/LearnWithLlew/ManyOfOne.slides)
+
---
+++ src/test/java/org/manyofone/_1_InMethod.java
@@ -0,0 +1,62 @@
+package org.manyofone;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _1_InMethod {
+    @Test
+    public void test1() {
+        assertEquals("1,2,3,5,7,11,13,", printPrimes());
+    }
+
+    /**
+     * Convert variable to many of one
+     */
+    private String printPrimes() {
+        String output = "";
+        int number1 = 1;
+        output += number1 + ",";
+        int number2 = 2;
+        output += number2 + ",";
+        int number3 = 3;
+        output += number3 + ",";
+        int number4 = 5;
+        output += number4 + ",";
+        int number5 = 7;
+        output += number5 + ",";
+        int number6 = 11;
+        output += number6 + ",";
+        int number7 = 13;
+        output += number7 + ",";
+        return output;
+    }
+
+
+    @Test
+    public void test2() {
+        assertEquals("1,2,3,4,5,6,7,", printNumbersUpTo7());
+    }
+
+    /**
+     * Convert statement to loop of many of one
+     */
+    private String printNumbersUpTo7() {
+        String output = "";
+        int number1 = 1;
+        output += number1 + ",";
+        int number2 = 2;
+        output += number2 + ",";
+        int number3 = 3;
+        output += number3 + ",";
+        int number4 = 4;
+        output += number4 + ",";
+        int number5 = 5;
+        output += number5 + ",";
+        int number6 = 6;
+        output += number6 + ",";
+        int number7 = 7;
+        output += number7 + ",";
+        return output;
+    }
+}
+
