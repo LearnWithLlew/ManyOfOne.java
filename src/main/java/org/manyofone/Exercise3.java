97bbee67b7e420804f8325fda332a32bcb67a364
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:34:32 -0600

    r added dead code

--- src/main/java/org/manyofone/Exercise3.java
+++ src/main/java/org/manyofone/Exercise3.java
@@ -2,6 +2,7 @@ package org.manyofone;
 
 public class Exercise3 {
     public static  String format(int number) {
+        FormatOptions options = new FormatOptions();
         return "#" + number;
     }
 }
