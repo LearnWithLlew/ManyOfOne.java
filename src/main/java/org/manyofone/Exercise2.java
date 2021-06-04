69473615ee18e0cb0cd815d52831bf5f230b1888
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:30:44 -0600

    r loop

--- src/main/java/org/manyofone/Exercise2.java
+++ src/main/java/org/manyofone/Exercise2.java
@@ -1,10 +1,12 @@
 package org.manyofone;
 
 public class Exercise2 {
-    public static String merge(String prefix, int... number)
-    {
+    public static String merge(String prefix, int... number) {
         String output = "";
-        output = prefix + number[0] + "\n";
+        for (int i : number) {
+
+            output = prefix + number[0] + "\n";
+        }
         return output;
     }
 }
