3cd117f0fce178759a12b837e51e060fdcc1cefe
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:30:11 -0600

    r split

--- src/main/java/org/manyofone/Exercise2.java
+++ src/main/java/org/manyofone/Exercise2.java
@@ -3,7 +3,8 @@ package org.manyofone;
 public class Exercise2 {
     public static String merge(String prefix, int... number)
     {
-        String output = prefix + number[0] + "\n";
+        String output;
+        output = prefix + number[0] + "\n";
         return output;
     }
 }
