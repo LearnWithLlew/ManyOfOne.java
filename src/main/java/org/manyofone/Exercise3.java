3ee8612e3fca48882c6077e46e341e558ca8690f
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:35:24 -0600

    r added optional variable

--- src/main/java/org/manyofone/Exercise3.java
+++ src/main/java/org/manyofone/Exercise3.java
@@ -3,6 +3,10 @@ package org.manyofone;
 public class Exercise3 {
     public static  String format(int number) {
         FormatOptions options = new FormatOptions();
+        return format(number,options);
+    }
+
+    public static String format(int number, FormatOptions options) {
         return "#" + number;
     }
 }
