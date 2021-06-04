9a77c3d2ec58f925aba65f07ef7ea75520a145e0
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:36:45 -0600

    r extract field

--- src/main/java/org/manyofone/FormatOptions.java
+++ src/main/java/org/manyofone/FormatOptions.java
@@ -1,8 +1,12 @@
 package org.manyofone;
 
 public class FormatOptions {
+
+    public String perfix;
+
     public String getPrefix() {
-        return "#";
+        perfix = "#";
+        return perfix;
 
     }
 }
