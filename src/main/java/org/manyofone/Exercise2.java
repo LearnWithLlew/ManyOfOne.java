a783f05992ce8573a38957ca5b2400defd0c938d
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:30:22 -0600

    r initalize

--- src/main/java/org/manyofone/Exercise2.java
+++ src/main/java/org/manyofone/Exercise2.java
@@ -3,7 +3,7 @@ package org.manyofone;
 public class Exercise2 {
     public static String merge(String prefix, int... number)
     {
-        String output;
+        String output = "";
         output = prefix + number[0] + "\n";
         return output;
     }
