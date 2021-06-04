f2d0a439328bbbf512b35ca37691e7218ace9a8a
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:36:57 -0600

    r move assingment

--- src/main/java/org/manyofone/FormatOptions.java
+++ src/main/java/org/manyofone/FormatOptions.java
@@ -2,10 +2,9 @@ package org.manyofone;
 
 public class FormatOptions {
 
-    public String perfix;
+    public String perfix = "#";
 
     public String getPrefix() {
-        perfix = "#";
         return perfix;
 
     }
