1f87361dc7d2c3233160aa35376ec8e2af5026b7
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-04 10:24:51 -0600

    R many of all

--- src/main/java/org/manyofone/Exercise1.java
+++ src/main/java/org/manyofone/Exercise1.java
@@ -6,22 +6,12 @@ public class Exercise1 {
      */
     public static String printPrimes() {
         String output = "";
-        int[] number1 = {1,2};
+        int[] number1 = {1,2,3,5,7,11,13};
         for (int i : number1) {
             output += i + ",";
 
         }
 
-        var number3 = 3;
-        output += number3 + ",";
-        var number4 = 5;
-        output += number4 + ",";
-        var number5 = 7;
-        output += number5 + ",";
-        var number6 = 11;
-        output += number6 + ",";
-        var number7 = 13;
-        output += number7 + ",";
         return output;
     }
 
